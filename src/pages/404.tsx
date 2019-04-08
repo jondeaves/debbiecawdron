11172ad7728aec0388675ef1f0ee75c0847c1bae
Jon Deaves <jon.deaves@fanduel.com>
2019-04-08 15:44:46 +0100

    feat: update 404 page styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
+import Container from '../components/shared/Container';
 import Layout from '../components/shared/Layout';
+import Link from '../components/shared/Link';
 import SEO from '../components/shared/Seo';
 
-export const NotFoundPage: React.FunctionComponent<{}> = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+export const NotFoundPage: React.FunctionComponent<{}> = () => {
+  return (
+    <Layout>
+      <SEO template="Not found | %s" />
+
+      <Container alignment="center" flow="column" type="section">
+        <h1>Page not Found</h1>
+        <p>
+          That page doesn't exist, why not{' '}
+          <Link to="/work" label="checkout some of my work" />
+        </p>
+      </Container>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
